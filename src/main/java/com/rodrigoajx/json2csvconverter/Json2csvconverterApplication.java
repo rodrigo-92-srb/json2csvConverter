9289262c487af3114a301cbd89cfda7950f68591
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-11-20 12:22:09 -0300

    Starting project and setup dependencies

---
+++ src/main/java/com/rodrigoajx/json2csvconverter/Json2csvconverterApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoajx.json2csvconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Json2csvconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Json2csvconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigoajx/json2csvconverter/Json2csvconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoajx.json2csvconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Json2csvconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
